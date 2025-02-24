123e1102a671309127a9e05eb2b67150bf835daf
lily <lily.you@student.tdsb.on.ca>
2025-02-24 20:10:49 +0000

    Comepleted Review 11

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,5 +4,38 @@ public class PracticeProblem {
 
 	}
 
+	public static int[] createIntArray(){
+		int[] arr = new int[5];
+		for(int i = 0; i < 5; i++){
+			arr[i] = i+1;
+		}
+		return arr;
+	}
 
+	public static String[] createArray(String word, String word2, String word3, String word4){
+		String[] arr = {word, word2, word3, word4};
+		return arr;
+	}
+
+	public static int findValue(int num, int[] arr){
+		for(int i = 0; i < arr.length; i++){
+			if(arr[i] == num){
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static int findThirdValue(String word, String[] arr){
+		int count = 0;
+		for(int i = 0; i < arr.length; i++){
+			if(arr[i] == word){
+				count++;
+				if(count == 3){
+					return i;
+				}
+			}
+		}
+		return -1;
+	}
 }
